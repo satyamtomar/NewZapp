5802c4fe9c39f2e0de89995322d2249ee8738ff2
satyamtomar <satyamtomar@users.noreply.github.com>
2021-09-20 23:44:47 +0530

    NewZapp

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import NewsArea from './components/NewsArea';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <NewsArea/>
+      </div>
+    )
+  }
+}
