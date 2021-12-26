5a0f4f2e196a8c29777e280fbcb963e6afc09cd9
satyamtomar <satyamtomar@users.noreply.github.com>
2021-12-26 17:02:34 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './nav.css'
 import {GiNewspaper } from "react-icons/gi"
 // You have to add group-hover variant in tailwind config
 // to see what I've done in tailwind config go check my git code. 
@@ -13,13 +13,26 @@ const Navbar = () => {
     <>
     
     <nav className="">
-    <div className="sticky top-0">
-
-    <div className=" bg-gray-300 items-center flex md:justify-between py-3 justify-center">
-     <div className=" flex flex-col  items-center space-x-8 md:flex-row">
+    
+    
+     <div className="bg-gray-800   py-3  text-gray-400 flex flex-col  items-center space-x-8 md:flex-row mx-auto justify-between px-3">
     <div className="flex flex-row items-center space-x-2 ">
-    <GiNewspaper className="h-8 w-8" />
-    <Link className="text-lg" to="/">NewZapp</Link>
+    
+    <svg viewBox="0 0 960 200" className="h-full w-full items-center">
+                <symbol id="s-text">
+                  <text text-anchor="middle" x="30%" y="60%" className="text-8xl">
+                  <Link className="hover:text-gray-600 " to="/">NewZapp</Link>
+                  
+                  </text>
+                </symbol>
+                <g className = "g-ants">
+		<use href="#s-text" class="text-copy"></use>
+		<use href="#s-text" class="text-copy"></use>
+		<use href="#s-text" class="text-copy"></use>
+		<use href="#s-text" class="text-copy"></use>
+		<use href="#s-text" class="text-copy"></use>
+	</g>
+              </svg>
     
     </div>
     <div className="flex items-center space-x-2 md:flex-row flex-col">
@@ -27,9 +40,7 @@ const Navbar = () => {
     </div>
     </div>
     
-
-    </div>
-    </div>
+    
 
     </nav>
 
